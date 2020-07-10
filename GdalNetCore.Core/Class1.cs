a01d5e2a755311fe98383bbc28c86d28b16712dc
bertt <btemme@hotmail.com>
2020-07-10 09:53:57 +0200

    add web project + reorder

---
+++ GdalNetCore.Core/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OSGeo.GDAL;
+using OSGeo.OGR;
+using OSGeo.OSR;
+
+namespace GdalNetCore.Core
+{
+    public static class Class1
+    {
+        public static string GetHello()
+        {
+            Gdal.AllRegister();
+            Ogr.RegisterAll();
+
+            return "hallo" + Ogr.GetDriverCount();
+        }
+    }
+}
