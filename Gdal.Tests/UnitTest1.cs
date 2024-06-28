67defb44cc9078768319ef3ae397ee031bdcc90a
Bert Temme <btemme@hotmail.com>
2024-06-28 22:11:42 +0200

    print platform

--- Gdal.Tests/UnitTest1.cs
+++ Gdal.Tests/UnitTest1.cs
@@ -10,8 +10,10 @@ public class Tests
         var kit = new GdalKit();
         var info = kit.GetGdalInfo();
         Assert.That(info.ReleaseName == "3.9.0");
-        Console.WriteLine("GDAL Drivers: " + String.Join(',', info.GdalDrivers));
+        Console.WriteLine("OS: " + Environment.OSVersion.Platform);
         Console.WriteLine("Gdal drivers: " + info.GdalDrivers.Count);
+        info.GdalDrivers.Sort();
+        Console.WriteLine("GDAL Drivers: " + String.Join(',', info.GdalDrivers);
 
         // if windows, count is 150 else 151
         if (Environment.OSVersion.Platform == PlatformID.Win32NT)
