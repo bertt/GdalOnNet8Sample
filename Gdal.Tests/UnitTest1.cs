fbefc15ec72f4fbf30733eca30c0b0af9af49db0
Bert Temme <btemme@hotmail.com>
2024-06-28 21:44:40 +0200

    add check gdal drivers

--- Gdal.Tests/UnitTest1.cs
+++ Gdal.Tests/UnitTest1.cs
@@ -9,9 +9,22 @@ public class Tests
     {
         var kit = new GdalKit();
         var info = kit.GetGdalInfo();
-        Assert.That(info.ReleaseName=="3.9.0");
+        Assert.That(info.ReleaseName == "3.9.0");
+        Console.WriteLine("GDAL Drivers: " + String.Join(',', info.GdalDrivers));
         Console.WriteLine("Gdal drivers: " + info.GdalDrivers.Count);
+
+        // if windows, count is 150 else 151
+        if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+        {
+            Assert.That(info.GdalDrivers.Count == 150);
+        }
+        else { 
+            Assert.That(info.GdalDrivers.Count == 151);
+        }
+
+
         Assert.That(info.GdalDrivers.Count == 150);
+        Console.WriteLine("OGR Drivers: " + String.Join(',', info.OgrDrivers));
         Console.WriteLine("Ogr drivers: " + info.OgrDrivers.Count);
         Assert.That(info.OgrDrivers.Count == 81);
     }
